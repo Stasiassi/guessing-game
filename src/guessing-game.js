61c987f774b33064ecd6fc2b0e0afbf9e4f7ec6a
Nastya <simonovanastasia360@gmail.com>
2023-02-25 08:20:13 +0300

    feat: make guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
 
-    setRange(min, max) {
+    }
 
+    setRange(mi, ma) {
+        this.min = mi;
+        this.max = ma;
     }
 
     guess() {
-
+        this.setRange( this.min,  this.max);
+        return Math.round(( this.min +  this.max)/2);
     }
 
     lower() {
-
+        this.max = Math.round(( this.min +  this.max)/2);
+        this.setRange( this.min,  this.max);
     }
 
     greater() {
-
+        this.min = Math.round(( this.min +  this.max)/2);
+        this.setRange( this.min,  this.max);
     }
 }
 
